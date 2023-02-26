62fefed2b2b62fb4b893c9df443c7bcbc5726dbb
baaddison1 <125115081+baaddison1@users.noreply.github.com>
2023-02-26 00:16:20 +0000

    Add files via upload

---
+++ Assignment 3(0).py
@@ -0,0 +1,66 @@
+#Index number:6922521
+#Script for the name and price of specific types of cars
+List_cars=['Camry','Toyota Prius','Toyota Corolla','Rav4','GR Supra','Altima','Kicks','Sentra','Maxima','Rogue','R Class','Mercedes-Benz B-Klass','Mercedes-Benz EQB','Mercedes-Benz GLB','Ferrari F430','Ferrari F40',\
+'Ferrari 599','Ferrari 275','Ferrari Daytoma','Honda CR-V','Honda Civic','Honda Accord','Pilot','Honda City','Continental GT','Bentley Flying Spur','Bentley R Type','Bentley Azure','Bentley S1','Mercedes Amg Gt' ]
+OPTIONS=print(list(List_cars))
+Question1=input('Select the car of your choice:')
+if Question1 == 'Camry':
+    print('The price of Camry is $300,000.00')
+elif Question1 =='Toyota Prius':
+    print('The price of Toyota Prius is $359,000.00')
+elif Question1 =='Toyota Corolla': 
+    print('The price of Toyota Corolla is $220,000.00')
+elif Question1 == 'Rav4' :
+    print('The price of Rav4 is $215,000.00')
+elif Question1 == 'GR Supra':
+    print('The price of GR Supra is $280,000.00')
+elif Question1 =='Altima':
+    print('The price of Altima is $290,000.00')
+elif Question1 == 'Kicks':
+    print('The price of Kicks is $420,000.00')
+elif Question1 =='Sentra':
+    print('The price of Sentra is $222,000.00')
+elif Question1 =='Maxima':
+    print('The price of Maxima is $320,000.00')
+elif Question1 =='Rogue':
+    print('The price of Rogue is $292,000.00')
+elif Question1 =='R Class':
+    print('The price of R Class is $204,000.00')
+elif Question1 =='Mercedes-Benz B-Klass':
+    print('The price of Mercedes-Benz B-Klass is $331,000.00')
+elif Question1 =='Mercedes-Benz EQB':
+    print('The price of Mercedes-Benz EQB is $400,000.00')
+elif Question1 =='Ferrari F430':
+    print('The price of Ferrari F430 is $240,000.00')
+elif Question1 =='Ferrari F40':
+    print('The price of Ferrari F40 is $380,000.00')    
+elif Question1 =='Ferrari 599':
+    print('The price of Ferrari 599 is $520,000.00')
+elif Question1 =='Ferrari 275':
+    print('The price of Ferrari 275 is $403,000.00')
+elif Question1 =='Ferrari Daytoma':
+    print('The price of Ferrari Daytoma is $412,000.00')
+elif Question1 =='Honda CR-V':
+    print('The price of Honda CR-V is $508,000.00')
+elif Question1 =='Honda Civic':
+    print('The price of Honda Civic is $211,000.00')
+elif Question1 =='Honda Accord':
+    print('The price of Honda Accord is $334,000.00')
+elif Question1 =='Pilot':
+    print('The price of Pilot is $270,000.00')
+elif Question1 =='Honda City':
+    print('The price of Honda City is $299,000.00') 
+elif Question1 =='Continental GT':
+    print('The price of Continental GT is $454,000.00')
+elif Question1 =='Bentley Flying Spur':
+    print('The price of Bentley Flying Spur is $370,000.00')
+elif Question1 =='Bentley R Type':
+    print('The price of Bentley R Type is $511,000.00')
+elif Question1 =='Bentley Azure':
+    print('The price of Bentley Azure is $422,000.00')
+elif Question1 =='Bentley S1':
+    print('The price of Bentley S1 is $342,000.00')
+elif Question1 =='Mercedes Amg Gt':
+    print('The price of Mercedes Amg Gt is $559,000.00')
+else:print('Sorry your choice of car is not available')
+    
